e8df44704b5dedd4e1e805b3c3327f3f8d199048
NicholasCH <nicholasch96@gmail.com>
2022-03-30 20:59:23 -0500

    reassessing self

--- server.js
+++ server.js
@@ -1,2 +1,23 @@
-const PORT = process.env.PORT || 3001;
 const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const apiRoutes = require('.routes/api-routes');
+const htmlRoutes = require('./routes/html-routes');
+const fs= require('fs');
+const path = require ('path');
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
